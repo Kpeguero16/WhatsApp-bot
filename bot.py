b0d8b116a3346fa1248a0b2d4ce116be2b00bc87
Khalil <khalilpeguero@hotmail.com>
2024-01-13 00:24:24 -0500

    Added time module

--- bot.py
+++ bot.py
@@ -1,4 +1,22 @@
 import pywhatkit
-import openai
+import time
 
-pywhatkit.sendwhatmsg("+19544129214", "Remember to check Canvas", 20, 40, 30)
+# Get the current time as a struct_time object
+current_time_struct = time.localtime()
+
+# Extract the hour and minute components
+current_hour = current_time_struct.tm_hour
+current_minute = current_time_struct.tm_min
+
+message = "test message. Sorry Maria."
+while True:
+    current_hour = time.localtime().tm_hour
+    current_minute = time.localtime().tm_min
+    print(current_hour, ":" ,current_minute)
+    if current_hour == 0 and current_minute == 7:
+        pywhatkit.sendwhatmsg_to_group("DMxYjJSzauBBEiPwerV9fd", message, current_hour, current_minute+1, 30, True, 5)
+        break
+    time.sleep(2)
+
+
+
